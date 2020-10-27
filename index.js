ccdef4ff2a3338e1f11b4e09ca43d5fb73585902
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-27 16:44:49 -0400

    added chalk red

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 
+const chalk = require('chalk');
+
 const fs = require('fs');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err,data) {
     if (err) { console.log(err) }
-    else { console.log(data); return data; }
+    else { console.log(chalk.red(data)); return data; }
 })
